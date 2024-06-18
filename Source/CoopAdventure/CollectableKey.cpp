abfafd8a0b27394aa96c5aa4681acad77989e6b3
Nick-Vyvyan <vyvyannick@gmail.com>
2024-06-17 17:19:53 -0700

    create collectable key class

---
+++ Source/CoopAdventure/CollectableKey.cpp
@@ -0,0 +1,61 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CollectableKey.h"
+#include "Components/CapsuleComponent.h"
+#include "Net/UnrealNetwork.h"
+
+// Sets default values
+ACollectableKey::ACollectableKey()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	bReplicates = true;
+	SetReplicateMovement(true);
+
+	RootComp = CreateDefaultSubobject<USceneComponent>(TEXT("RootComp"));
+	SetRootComponent(RootComp);
+
+	Capsule = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule"));
+	Capsule->SetupAttachment(RootComp);
+	Capsule->SetIsReplicated(true);
+	Capsule->SetCollisionProfileName("OverlapAllDynamic");
+	Capsule->SetCapsuleHalfHeight(150.f);
+	Capsule->SetCapsuleRadius(100.f);
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	Mesh->SetupAttachment(RootComp);
+	Mesh->SetIsReplicated(true);
+	Mesh->SetCollisionProfileName(FName("OverlapAllDynamic"));
+
+}
+
+void ACollectableKey::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(ACollectableKey, bIsCollected);
+}
+
+void ACollectableKey::OnRep_IsCollected()
+{
+
+}
+
+
+// Called when the game starts or when spawned
+void ACollectableKey::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+// Called every frame
+void ACollectableKey::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+
